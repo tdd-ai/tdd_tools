69d5c32cc6c20b5abb186bd94b427f67d42cb039
Taner Sezer <tanersezerr@gmail.com>
2023-03-27 00:17:24 +0300

    Update ngram.py
    Updates

--- ngram.py
+++ ngram.py
@@ -1,23 +1,23 @@
 import re
+from collections import Counter
+from unicode_tr import unicode_tr
 
-def turk_lower(word):
-    word = word.replace("İ", "i")
-    word = word.replace("I", "ı")
-    return word.lower()
+def preprocess(text):
+    # Lowercase and remove special characters
+    text = unicode_tr(text).lower()
+    text = re.sub(r'[^a-zşğıiüöç\n ]', '', text)
+    return text
 
-def white_spaced(text):
-    txt = re.sub(r'([a-zşğıiüöçA-ZŞĞIİÜÖÇ])([?,.!"“”;:])', r'\1 \2 ', text)
-    return txt
+def generate_ngrams(text, size):
+    # Tokenize and generate n-grams
+    tokens = preprocess(text).strip().split()
+    ngrams_list = [tuple(tokens[num:num + size]) for num in range(0, len(tokens) - size + 1)]
+    # Count frequencies
+    ngrams_freq = Counter(ngrams_list)
+    return ngrams_freq
 
-def gen_ngram(text, size):
-    ngrams_list = []
-    tokens = white_spaced(turk_lower(text))
-    tokens = re.sub(r" {2,}", " ", tokens)
-    tokens = re.sub(r"\t{1,}", " ", tokens)
-    tokens = re.sub(r"\n{1,}", " ", tokens)
-    tokens = re.sub(r"\r{1,}", " ", tokens)
-    text = turk_lower(white_spaced(tokens).replace("\n", " ")).strip(" ").split()
-    for num in range(0, len(text)):
-        ngram = text[num:num + size]
-        ngrams_list.append(ngram)
-    return ngrams_list[:-(size-1)]
+#s_text = "Bu bir deneme metnidir. Deneme metinleri, bir yazılımın doğru ve düzgün çalışıp çalışmadığını test etmek için kullanılır. Bu n-gram örneği bir yazılımın doğru 3'lük grupları hesaplar."
+#size = 3
+#ngrams_freq = generate_ngrams(s_text, size)
+#for ngram, freq in ngrams_freq.items():
+#    print(ngram, freq)
